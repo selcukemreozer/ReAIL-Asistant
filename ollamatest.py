06666c85af735152168b512e89cf5b456fe9adfa
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2025-01-01 00:28:23 +0300

    llama vision model test

--- ollamatest.py
+++ ollamatest.py
@@ -1,10 +1,32 @@
 import ollama
-response = ollama.chat(model="gemma2:2b", messages=[
-                           {
-                               'role': 'user',
-                               'content': 'Hello, how are you?'
-                           }
-                       ]
-                       )
+from time import time
+import pyautogui as pag
 
-print(response['message']['content'])
+
+# resolution of the screen
+screenWidth, screenHeight = pag.size()
+print("Screen resolution: ", screenWidth, screenHeight)
+#start
+start = time()
+response = ollama.chat(
+    model='llama3.2-vision',
+    messages=[{
+        'role': 'user',
+        'content': f'''This image is a screenshot from my computer. The top left corner is (0,0). The bottom right corner is (1440,900).
+        On some points of the image, there are their coordinate info. Use them as reference points. 
+        Give me pixel coordinates of the mocomtool file
+        in the image resolution of {screenWidth}x{screenHeight}.
+        I do not need explanation, just the one coordinate. Example response: (100, 200)''',
+        'images': ['image.png']
+    }]
+)
+
+print(response)
+end = time()
+# two decimal places
+print("Time taken: ", round(end-start, 2))
+print(response)
+
+'''This image is a screenshot from my computer. Give me pixel coordinates of the apple logo
+                                           in the image resolution of {screenWidth}x{screenHeight}.
+                                           I do not need explanation, just the one coordinate. Example response: (100, 200)''',
